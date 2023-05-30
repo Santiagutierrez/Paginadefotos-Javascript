568d5601438dbed3f6878d052138f7b2a615de49
Santiago Gutierrez <santiagogutierrezcode@gmail.com>
2023-05-29 23:46:35 -0300

    entrega 3

---
+++ JS/products.js
@@ -0,0 +1,30 @@
+const productos = [
+    {
+        id:1,
+        nombre: "nikkor",
+        precio: 50000,
+        img: "https://www.ds-km.com/wp-content/uploads/2020/06/Nikon_FM2n-1024x684.jpg",
+        cantidad: 1,
+    },
+    {
+        id:2,
+        nombre: "Canon",
+        precio: 15000,
+        img: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/80/Canon_a1.JPG/435px-Canon_a1.JPG",
+        cantidad: 1,
+    },
+    {
+        id:3,
+        nombre: "Pentax",
+        precio: 80000,
+        img: "https://p.turbosquid.com/ts-thumb/2Y/whCnos/UHGXE2QO/pentax_k1000_600_0001/jpg/1534164811/1920x1080/fit_q99/ae68f2399ca1b30efa7d95d462f3f282f10614f1/pentax_k1000_600_0001.jpg",
+        cantidad: 1,
+    },
+    {
+        id:4,
+        nombre: "Zenit",
+        precio: 24000,
+        img: "https://assets.community.lomography.com/4e/604fa2d3236fa2b75d83da7c2c59fb872636d5/310x307x2.jpg?auth=fff17e35d46a8f2c4c9a0a0431ab09afbb488517",
+        cantidad: 1,
+    },
+];
